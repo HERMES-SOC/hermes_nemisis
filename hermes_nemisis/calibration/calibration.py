3a9e183b25886b4834c29a0a2203cbf1abdb17af
BROMUND, KENNETH R 014900503 <kenneth.r.bromund@nasa.gov>
2024-06-06 17:05:37 +0000

    all funtionality of readbin_CCSDS.py is now implemented within calibration.py, using CCSDSpy

--- hermes_nemisis/calibration/calibration.py
+++ hermes_nemisis/calibration/calibration.py
@@ -16,7 +16,7 @@ from hermes_nemisis.io import read_file
 __all__ = [
     "process_file",
     "parse_l0_sci_packets",
-    "l0_sci_data_to_cdf",
+    "l0_sci_data_to_l1",
     "calibrate_file",
     "get_calibration_file",
     "read_calibration_file",
@@ -72,9 +72,6 @@ def calibrate_file(data_filename: Path) -> Path:
     >>> level1_file = calibrate_file('hermes_NEM_l0_2022239-000000_v0.bin')  # doctest: +SKIP
     """
     log.info(f"Calibrating file:{data_filename}.")
-    output_filename = (
-        data_filename  # TODO: for testing, the output filename MUST NOT same as input
-    )
     file_metadata = parse_science_filename(data_filename.name)
 
     # check if level 0 binary file, if so call appropriate functions
@@ -82,12 +79,8 @@ def calibrate_file(data_filename: Path) -> Path:
         file_metadata["instrument"] == hermes_nemisis.INST_NAME
         and file_metadata["level"] == "l0"
     ):
-        #  data = parse_l0_sci_packets(data_filename)
-        data = {}
-        # test opening the file
-        with open(data_filename, "r") as fp:
-            pass
-        level1_filename = l0_sci_data_to_cdf(data, data_filename)
+        data = parse_l0_sci_packets(data_filename)
+        level1_filename = l0_sci_data_to_l1(data, data_filename)
         output_filename = level1_filename
     elif (
         file_metadata["instrument"] == hermes_nemisis.INST_NAME
@@ -159,7 +152,7 @@ def parse_l0_sci_packets(data_filename: Path) -> dict:
     return data
 
 
-def l0_sci_data_to_cdf(data: dict, original_filename: Path) -> Path:
+def l0_sci_data_to_l1(data: dict, original_filename: Path) -> Path:
     """
     Write level 0 nemisis science data to a level 1 cdf file.
 
@@ -183,22 +176,236 @@ def l0_sci_data_to_cdf(data: dict, original_filename: Path) -> Path:
     >>> data_filename = Path("hermes_NEM_l0_2022339-000000_v0.bin")
     >>> metadata = parse_science_filename(data_filename)  # doctest: +SKIP
     >>> data_packets = calib.parse_l0_sci_packets(data_filename)  # doctest: +SKIP
-    >>> cdf_filename = calib.l0_sci_data_to_cdf(data_packets, data_filename)  # doctest: +SKIP
+    >>> cdf_filename = calib.l0_sci_data_to_l1(data_packets, data_filename)  # doctest: +SKIP
     """
-    file_metadata = parse_science_filename(original_filename.name)
+    ####  
+    #### KRB: note that this code from the template is not used.
+    ####      the file name is created by HermesData.save().
+    ####      What we want to do is figure out the version number and 
+    ####      time stamp for the L1 data file, IF they depends on the
+    ####      L0 version number.
+    # file_metadata = parse_science_filename(original_filename.name)
 
-    cdf_filename = original_filename.parent / create_science_filename(
-        file_metadata["instrument"],
-        file_metadata["time"],
-        "l1",
-        f'1.0.{file_metadata["version"]}',
+    # cdf_filename = original_filename.parent / create_science_filename(
+    #     file_metadata["instrument"],
+    #     file_metadata["time"],
+    #     "l1",
+    #     f'1.0.{file_metadata["version"]}',
+    # )
+    ######
+
+    from astropy.time import Time, TimeDelta
+    import astropy.units as u
+    from astropy.timeseries import TimeSeries
+    import numpy as np
+
+    pckt_times = []
+    for pckt in range(len(data['MET_SECONDS'])):
+        pckt_times.append(data['MET_SECONDS'][pckt]+data['SCTF_SECONDS'][pckt]+data['SCTF_SUBSECONDS'][pckt]/2**32)
+
+    pckt_delta = np.median(np.diff(pckt_times))
+    fg_times = []
+    fg_bx = []
+    fg_by = []
+    fg_bz = []
+    pni1_bx = []
+    pni1_by = []
+    pni1_bz = []
+    pni2_bx = []
+    pni2_by = []
+    pni2_bz = []
+    for pckt in range(len(pckt_times)):
+        for i in range(40):
+            sample_time = pckt_times[pckt] + pckt_delta * (i - data['ACQ_POS_NUM'][pckt]) / 40 - data['ISR_COUNT'][pckt]*14.933e-6
+            fg_times.append(sample_time)
+            fg_bx.append(data['MAG_DATA'][pckt][i*9 ])
+            fg_by.append(data['MAG_DATA'][pckt][i*9 +1])
+            fg_bz.append(data['MAG_DATA'][pckt][i*9 +2])
+
+            pni1_bx.append(data['MAG_DATA'][pckt][i*9 +3])
+            pni1_by.append(data['MAG_DATA'][pckt][i*9 +4])
+            pni1_bz.append(data['MAG_DATA'][pckt][i*9 +5])
+
+            pni2_bx.append(data['MAG_DATA'][pckt][i*9 +6])
+            pni2_by.append(data['MAG_DATA'][pckt][i*9 +7])
+            pni2_bz.append(data['MAG_DATA'][pckt][i*9 +8])
+    
+    # convert fluxgate to pseudo-nT
+    fg_bx = np.array(fg_bx) / 8388608.0 * 65000.0     
+    fg_by = np.array(fg_by) / 8388608.0 * 65000.0
+    fg_bz = np.array(fg_bz) / 8388608.0 * 65000.0
+
+    # convert PNI to pseudo-nT
+    pni1_bx = np.array(pni1_bx) * 13.66 / 7.0            
+    pni1_by = np.array(pni1_by) * 13.66 / 7.0
+    pni1_bz = np.array(pni1_bz) * 13.66 / 7.0
+    pni2_bx = np.array(pni2_bx) * 13.66 / 7.0            
+    pni2_by = np.array(pni2_by) * 13.66 / 7.0
+    pni2_bz = np.array(pni2_bz) * 13.66 / 7.0
+
+    # temperature conversions to engineering units
+    t_eb = np.array(data['EBOX_TEMP']) / 128 
+    t_fg = np.array(data['FG_TEMP']) * (3.3 / 1024) * 100 - 273.15
+    t_pni1 = np.array(data['PNI1_TEMP']) # PNI MI-S1 sensor temperature conversion **GOES HERE**
+    t_pni2 = np.array(data['PNI2_TEMP']) # PNI MI-S2 sensor temperature conversion **GOES HERE**
+
+    # convert time tags to AstroPy times
+    tt2k_epoch = Time('2000-01-01 11:58:55.816', scale='utc')
+    pckt_time = tt2k_epoch + TimeDelta(pckt_times * u.s)
+    fg_time = tt2k_epoch + TimeDelta(fg_times * u.s)
+
+    # store 10 Sample/Sec (magnetometer cadence) data in AstroPy TimeSeries structures
+    magts = TimeSeries(
+      time=fg_time,
+      data={
+         'hermes_nem_fg_b1': u.Quantity(
+            value = fg_bx,
+            unit='nT',  # actually, 'pseudo-nT'  perhaps use u.def_unit('pseudo-nT') and u.add_enabled_units
+            dtype=np.float32
+         ),
+         'hermes_nem_fg_b2': u.Quantity(
+            value = fg_by,
+            unit='nT',
+            dtype=np.float32
+         ),
+         'hermes_nem_fg_b3': u.Quantity(
+            value = fg_bz,
+            unit='nT',
+            dtype=np.float32
+         ),
+
+         # the PNIs should have their own timeseries, with its own time tags
+         # otherwise, how do we account for the offset between the integration times?
+         'hermes_nem_pni1_b1': u.Quantity(
+            value = pni1_bx,
+            unit='nT',  # actually, 'pseudo-nT'  perhaps use u.def_unit('pseudo-nT') and u.add_enabled_units
+            dtype=np.float32
+         ),
+         'hermes_nem_pni1_b2': u.Quantity(
+            value = pni1_by,
+            unit='nT',
+            dtype=np.float32
+         ),
+         'hermes_nem_pni1_b3': u.Quantity(
+            value = pni1_bz,
+            unit='nT',
+            dtype=np.float32
+         ),
+
+         'hermes_nem_pni2_b1': u.Quantity(
+            value = pni2_bx,
+            unit='nT',  # actually, 'pseudo-nT'  perhaps use u.def_unit('pseudo-nT') and u.add_enabled_units
+            dtype=np.float32
+         ),
+         'hermes_nem_pni2_b2': u.Quantity(
+            value = pni2_by,
+            unit='nT',
+            dtype=np.float32
+         ),
+         'hermes_nem_pni2_b3': u.Quantity(
+            value = pni2_bz,
+            unit='nT',
+            dtype=np.float32
+         )
+
+      }
+   )
+
+    # store 1 Sample/4 Sec (packet cadence) data in AstroPy TimeSeries structures
+    pktts = TimeSeries(
+        time=pckt_time,
+        data={
+            'hermes_nem_ebox_temp': u.Quantity(
+            value = t_eb,
+            unit='deg C',
+            dtype=np.float32
+            ),
+            'hermes_nem_fg_temp': u.Quantity(
+            value = t_fg,
+            unit='deg C',
+            dtype=np.float32
+            ),
+            'hermes_nem_pni1_temp': u.Quantity(
+            value = t_pni1,
+            unit='deg C',
+            dtype=np.float32
+            ),
+            'hermes_nem_pni2_temp': u.Quantity(
+            value = t_pni2,
+            unit='deg C',
+            dtype=np.float32
+            )
+        }
     )
 
-    # create an empty file for testing purposes
-    with open(cdf_filename, "w"):
-        pass
+    from hermes_core.timedata import HermesData
+    from collections import OrderedDict
 
-    return cdf_filename
+    ts = {
+        'Epoch': magts,
+        'Epoch_pkt': pktts
+    }
+
+    # now flesh out the HermesData structure
+    input_attrs = HermesData.global_attribute_template("nemisis", "l1", "1.0.0")
+
+    nemisis_data = HermesData(
+        timeseries=ts,
+        meta = input_attrs
+    )
+
+    nemisis_data.timeseries["Epoch"]["hermes_nem_fg_b1"].meta.update(
+        OrderedDict({"CATDESC":"FG axis 1 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_fg_b2"].meta.update(
+        OrderedDict({"CATDESC":"FG axis 2 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_fg_b3"].meta.update(
+        OrderedDict({"CATDESC":"FG axis 3 OUTPUT IN PSEUDO-nT"})
+    )
+
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni1_b1"].meta.update(
+        OrderedDict({"CATDESC":"PNI1 axis 1 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni1_b2"].meta.update(
+        OrderedDict({"CATDESC":"PNI1 axis 2 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni1_b3"].meta.update(
+        OrderedDict({"CATDESC":"PNI1 axis 3 OUTPUT IN PSEUDO-nT"})
+    )
+
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni2_b1"].meta.update(
+        OrderedDict({"CATDESC":"PNI2 axis 1 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni2_b2"].meta.update(
+        OrderedDict({"CATDESC":"PNI2 axis 2 OUTPUT IN PSEUDO-nT"})
+    )
+    nemisis_data.timeseries["Epoch"]["hermes_nem_pni2_b3"].meta.update(
+        OrderedDict({"CATDESC":"PNI2 axis 3 OUTPUT IN PSEUDO-nT"})
+    )
+
+
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_ebox_temp"].meta.update(
+        OrderedDict({"CATDESC":"EBOX temperature in deg C"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_fg_temp"].meta.update(
+        OrderedDict({"CATDESC":"FG sensor temperature in deg C"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_pni1_temp"].meta.update(
+        OrderedDict({"CATDESC":"PNI1 sensor temperature in deg C"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_pni2_temp"].meta.update(
+        OrderedDict({"CATDESC":"PNI2 sensor temperature in deg C"})
+    )
+
+
+    cdf_file_path = nemisis_data.save(output_path=hermes_nemisis._data_directory, overwrite=True)
+    return cdf_file_path
+    # 
+#    cdf_file_path = nemisis_data.save(output_path=cdf_filename, overwrite=True)
+
+
+#    return cdf_filename
 
 
 def get_calibration_file(data_filename: Path, time=None) -> Path:
--- hermes_nemisis/tests/test_calibration.py
+++ hermes_nemisis/tests/test_calibration.py
@@ -5,10 +5,11 @@ import hermes_nemisis.calibration as calib
 from hermes_core.util.util import create_science_filename, parse_science_filename
 from hermes_nemisis.calibration.readbin_CCSDS import readbin_CCSDS
 
+hermes_nemisis_data_directory = Path('hermes_nemisis/data/')
 
-level0_filename = "hermes_NEM_l0_2022339-000000_v0.bin"
-level1_filename = "hermes_nem_l1_20221205T000000_v1.0.0.cdf"
-ql_filename = "hermes_nem_ql_20221205T000000_v1.0.0.cdf"
+level0_filename = "hermes_NEM_l0_2024085-174022_v00.bin"
+level1_filename = "hermes_nem_l1_20240325T173118_v1.0.0.cdf"
+ql_filename = "hermes_nem_ql_20240325T173118_v1.0.0.cdf"
 
 
 @pytest.fixture(scope="session")
@@ -26,14 +27,19 @@ def level1_file(tmp_path_factory):
         pass
     return fn
 
-def test_readbin_CCSDS():  
-    output_file = readbin_CCSDS('hermes_nemisis/data/hermes_NEM_l0_2024085-174022_v00.bin')
-    # assert output_file.is_file()  # this fails.  output_file is a string object, not a file. is this a HermesData issue?
+# def test_readbin_CCSDS():  
+#     output_file = readbin_CCSDS('hermes_nemisis/data/hermes_NEM_l0_2024085-174022_v00.bin')
+#     # assert output_file.is_file()  # this fails.  output_file is a string object, not a file. is this a HermesData issue?
 
-def test_nemisis_sci_data_to_cdf(level0_file):
+def test_parse_l0_sci_packets():
+    data = calib.parse_l0_sci_packets('hermes_nemisis/data/hermes_NEM_l0_2024085-174022_v00.bin')
+    assert data['MET_SECONDS'][0] == 2214
+    assert len(data['MET_SECONDS']) == 270 
+
+def test_nemisis_sci_data_to_l1(level0_file):
     """Test that the output filenames are correct and that a file was actually created."""
-    data = {}
-    output_file = calib.l0_sci_data_to_cdf(data, level0_file)
+    data = calib.parse_l0_sci_packets('hermes_nemisis/data/hermes_NEM_l0_2024085-174022_v00.bin')
+    output_file = calib.l0_sci_data_to_l1(data, level0_file)
     assert output_file.name == level1_filename
     assert output_file.is_file()
 
@@ -52,6 +58,7 @@ def test_process_file_nofile_error():
 
 def test_calibrate_file(level0_file, level1_file):
     """Test that the output filenames are correct and that a file was actually created."""
+#   NOTE the way the template is written, the simple file name is not sufficient
     output_file = calib.calibrate_file(level0_file)
     assert output_file.name == level1_filename
     assert output_file.is_file()
@@ -59,20 +66,20 @@ def test_calibrate_file(level0_file, level1_file):
     assert output_file.name == ql_filename
     assert output_file.is_file()
 
-    # with pytest.raises(ValueError) as excinfo:
-    #    calib.calibrate_file("datafile_with_no_calib.cdf")
-    # assert (
-    #    str(excinfo.value)
-    #    == "Calibration file for datafile_with_no_calib.cdf not found."
-    # )
+#     # with pytest.raises(ValueError) as excinfo:
+#     #    calib.calibrate_file("datafile_with_no_calib.cdf")
+#     # assert (
+#     #    str(excinfo.value)
+#     #    == "Calibration file for datafile_with_no_calib.cdf not found."
+#     # )
 
 
-def test_process_file_level0(level0_file):
-    """Test that the output filenames are correct and that a file was actually created."""
-    file_output = calib.process_file(level0_file)
-    assert len(file_output) == 1
-    assert file_output[0].name == level1_filename
-    assert file_output[0].is_file()
+# def test_process_file_level0(level0_file):
+#     """Test that the output filenames are correct and that a file was actually created."""
+#     file_output = calib.process_file(level0_file)
+#     assert len(file_output) == 1
+#     assert file_output[0].name == level1_filename
+#     assert file_output[0].is_file()
 
 
 def test_process_file_level1(level1_file):
