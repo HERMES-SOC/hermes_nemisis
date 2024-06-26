a78ac6fd99bfc77d5835d01f677ed0be82a41409
BROMUND, KENNETH R 014900503 <kenneth.r.bromund@nasa.gov>
2024-06-26 21:28:48 +0000

    calibration.py: * Raises a ValueError if a zero-length file is encountered, rather than an obsure CCSDSpy error about the 6 byte header length. * Calculates checksums using ccsdspy.utils.split_packet_bytes to retrieve the packet bytes. * Z-version of output file is taken from the version of the Level 0 input file. * calculates the packet time for initial packets with special operations status: RELAY_STATE == 0  OR  START_FLAG == 1  OR  XSUM == 0.  If the file concludes with this type of packet, then it gets tossed. * pre-allocates data arrays, rather than concatenating sample by sample… * decodes the RT clock, including Binary Coded Decimal conversion, and converting a GPS time for storage. * stores all packet status data (packet id, acqisition position number, etc.) in the CDF.

--- hermes_nemisis/calibration/calibration.py
+++ hermes_nemisis/calibration/calibration.py
@@ -6,6 +6,7 @@ import random
 from pathlib import Path
 
 import ccsdspy
+import numpy as np
 
 from hermes_core import log
 from hermes_core.util.util import create_science_filename, parse_science_filename
@@ -148,7 +149,27 @@ def parse_l0_sci_packets(data_filename: Path) -> dict:
     pkt = ccsdspy.FixedLength.from_file(
         Path(hermes_nemisis._data_directory) / "NEM_sci_packet_def.csv"
     )
+
+    if (data_filename.stat().st_size == 0):
+        raise ValueError(f"Zero length file: {data_filename}")
     data = pkt.load(data_filename)
+
+    # calculate checksum of each packet
+    n_pkts = len(data['CHECKSUM'])
+    packet_bytes = ccsdspy.utils.split_packet_bytes(
+        data_filename,
+        include_primary_header=False
+    )
+    calculated_checksum_out = np.zeros(n_pkts, dtype=np.uint16)
+    for i in range(n_pkts):
+        CK_A = 0 
+        CK_B = 0
+        for octet_num in range(1115):
+            CK_A = (CK_A + packet_bytes[i][octet_num+6]) % 255
+            CK_B = (CK_B + CK_A) % 255
+        calculated_checksum_out[i] = (CK_B << 8) | CK_A
+    data["calculated_checksum"] = calculated_checksum_out
+
     return data
 
 
@@ -178,81 +199,128 @@ def l0_sci_data_to_l1(data: dict, original_filename: Path) -> Path:
     >>> data_packets = calib.parse_l0_sci_packets(data_filename)  # doctest: +SKIP
     >>> cdf_filename = calib.l0_sci_data_to_l1(data_packets, data_filename)  # doctest: +SKIP
     """
-    ####  
-    #### KRB: note that this code from the template is not used.
-    ####      the file name is created by HermesData.save().
-    ####      What we want to do is figure out the version number and 
-    ####      time stamp for the L1 data file, IF they depends on the
-    ####      L0 version number.
-    # file_metadata = parse_science_filename(original_filename.name)
+    log.info(f"Begin l0_sci_data_to_l1")
 
-    # cdf_filename = original_filename.parent / create_science_filename(
-    #     file_metadata["instrument"],
-    #     file_metadata["time"],
-    #     "l1",
-    #     f'1.0.{file_metadata["version"]}',
-    # )
-    ######
+    file_metadata = parse_science_filename(original_filename.name)
+
+    cdf_version = f'1.0.{int(file_metadata["version"])}'
 
     from astropy.time import Time, TimeDelta
     import astropy.units as u
     from astropy.timeseries import TimeSeries
-    import numpy as np
 
-    pckt_times = []
-    for pckt in range(len(data['MET_SECONDS'])):
-        pckt_times.append(data['MET_SECONDS'][pckt]+data['SCTF_SECONDS'][pckt]+data['SCTF_SUBSECONDS'][pckt]/2**32)
+    n_pkts = len(data['MET_SECONDS'])
+    acq_pos_times = np.zeros(n_pkts)
+    acq_pos_num = np.zeros(n_pkts)
+    max_pkt = 0
+    # # test bad packet(s) at end of file #### TEST
+    # data["START_FLAG"][n_pkts-1] = 1    #### TEST
+    # data["XSUM"][n_pkts-2] = 0          #### TEST
+    for pkt in range(n_pkts):
+        # if RELAY_STATE == 0  OR  START_FLAG == 1  OR  XSUM == 0 then 
+        # we must not use the the time-at-tone, ISR count or ACQ_POS_NUM.  
+        if (data['RELAY_STATE'][pkt] == 1 and data['START_FLAG'][pkt] == 0 and data['XSUM'][pkt] == 1):
+            # these are the precise times of the ACQ_POS_NUM sample of each packet
+            # (we know the time at tone pulse occurs ISR_COUNT interrupts after the ACQ_POS_NUM'th sample is begun)
+            acq_pos_times[pkt] = data['MET_SECONDS'][pkt] + data['SCTF_SECONDS'][pkt] + (data['SCTF_SUBSECONDS'][pkt]/2**32) - \
+                (data['ISR_COUNT'][pkt]*14.933e-6)
+            acq_pos_num[pkt] = data['ACQ_POS_NUM'][pkt]
+            max_pkt = pkt
+        else:
+            log.info(f"ignoring packet time for PKT_ID {data['PKT_ID'][pkt]}, RELAY_STATE: {data['RELAY_STATE'][pkt]}, START_FLAG: {data['START_FLAG'][pkt]}, XSUM: {data['XSUM'][pkt]}")
 
-    pckt_delta = np.median(np.diff(pckt_times))
-    fg_times = []
-    fg_bx = []
-    fg_by = []
-    fg_bz = []
-    pni1_bx = []
-    pni1_by = []
-    pni1_bz = []
-    pni2_bx = []
-    pni2_by = []
-    pni2_bz = []
-    for pckt in range(len(pckt_times)):
+    samp_delta = np.median(np.diff(acq_pos_times))/40
+
+    # now find start-up packets and fill them in with time tags calculated from the following packet, 
+    # working backwards from the last valid packet
+    for pkt in range(max_pkt-1,-1,-1):
+        if (acq_pos_times[pkt] == 0):
+            acq_pos_times[pkt] = acq_pos_times[pkt+1] - samp_delta*40
+            acq_pos_num[pkt] = acq_pos_num[pkt+1]
+
+    # if the last one is bad, then we have to toss it:
+    if (max_pkt + 1 < n_pkts):
+        log.info(f"Truncating {n_pkts - max_pkt - 1} packets from end of file: PKT_IDs {data['PKT_ID'][range(max_pkt+1,n_pkts)]}")
+        n_pkts = max_pkt + 1
+        acq_pos_times = acq_pos_times[range(n_pkts)]
+        acq_pos_num = acq_pos_num[range(n_pkts)]
+
+    fg_times = np.zeros(n_pkts*40)
+    fg_bx =    np.zeros(n_pkts*40)
+    fg_by =    np.zeros(n_pkts*40)
+    fg_bz =    np.zeros(n_pkts*40)
+    pni1_bx =  np.zeros(n_pkts*40)
+    pni1_by =  np.zeros(n_pkts*40)
+    pni1_bz =  np.zeros(n_pkts*40)
+    pni2_bx =  np.zeros(n_pkts*40)
+    pni2_by =  np.zeros(n_pkts*40)
+    pni2_bz =  np.zeros(n_pkts*40)
+    for pkt in range(n_pkts):
         for i in range(40):
-            sample_time = pckt_times[pckt] + pckt_delta * (i - data['ACQ_POS_NUM'][pckt]) / 40 - data['ISR_COUNT'][pckt]*14.933e-6
-            fg_times.append(sample_time)
-            fg_bx.append(data['MAG_DATA'][pckt][i*9 ])
-            fg_by.append(data['MAG_DATA'][pckt][i*9 +1])
-            fg_bz.append(data['MAG_DATA'][pckt][i*9 +2])
+            sample_time = acq_pos_times[pkt] + samp_delta * (i - acq_pos_num[pkt])
+            fg_times[pkt*40+i] = sample_time
+            fg_bx[pkt*40+i] = data['MAG_DATA'][pkt][i*9 ]
+            fg_by[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +1]
+            fg_bz[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +2]
 
-            pni1_bx.append(data['MAG_DATA'][pckt][i*9 +3])
-            pni1_by.append(data['MAG_DATA'][pckt][i*9 +4])
-            pni1_bz.append(data['MAG_DATA'][pckt][i*9 +5])
+            pni1_bx[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +3]
+            pni1_by[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +4]
+            pni1_bz[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +5]
 
-            pni2_bx.append(data['MAG_DATA'][pckt][i*9 +6])
-            pni2_by.append(data['MAG_DATA'][pckt][i*9 +7])
-            pni2_bz.append(data['MAG_DATA'][pckt][i*9 +8])
+            pni2_bx[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +6]
+            pni2_by[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +7]
+            pni2_bz[pkt*40+i] = data['MAG_DATA'][pkt][i*9 +8]
     
+    # end time of each packet (in theory)
+    pkt_times = acq_pos_times + (40 - acq_pos_num)*samp_delta
+
+
     # convert fluxgate to pseudo-nT
-    fg_bx = np.array(fg_bx) / 8388608.0 * 65000.0     
-    fg_by = np.array(fg_by) / 8388608.0 * 65000.0
-    fg_bz = np.array(fg_bz) / 8388608.0 * 65000.0
+    fg_bx = fg_bx / 8388608.0 * 65000.0     
+    fg_by = fg_by / 8388608.0 * 65000.0
+    fg_bz = fg_bz / 8388608.0 * 65000.0
 
     # convert PNI to pseudo-nT
-    pni1_bx = np.array(pni1_bx) * 13.66 / 7.0            
-    pni1_by = np.array(pni1_by) * 13.66 / 7.0
-    pni1_bz = np.array(pni1_bz) * 13.66 / 7.0
-    pni2_bx = np.array(pni2_bx) * 13.66 / 7.0            
-    pni2_by = np.array(pni2_by) * 13.66 / 7.0
-    pni2_bz = np.array(pni2_bz) * 13.66 / 7.0
+    pni1_bx = pni1_bx * 13.66 / 7.0            
+    pni1_by = pni1_by * 13.66 / 7.0
+    pni1_bz = pni1_bz * 13.66 / 7.0
+    pni2_bx = pni2_bx * 13.66 / 7.0            
+    pni2_by = pni2_by * 13.66 / 7.0
+    pni2_bz = pni2_bz * 13.66 / 7.0
 
     # temperature conversions to engineering units
-    t_eb = np.array(data['EBOX_TEMP']) / 128 
-    t_fg = np.array(data['FG_TEMP']) * (3.3 / 1024) * 100 - 273.15
-    t_pni1 = np.array(data['PNI1_TEMP']) # PNI MI-S1 sensor temperature conversion **GOES HERE**
-    t_pni2 = np.array(data['PNI2_TEMP']) # PNI MI-S2 sensor temperature conversion **GOES HERE**
+    t_eb = np.array(data['EBOX_TEMP'][range(n_pkts)]) / 128 
+    t_fg = np.array(data['FG_TEMP'][range(n_pkts)]) * (3.3 / 1024) * 100 - 273.15
+    t_pni1 = np.array(data['PNI1_TEMP'][range(n_pkts)]) # PNI MI-S1 sensor temperature conversion **GOES HERE**
+    t_pni2 = np.array(data['PNI2_TEMP'][range(n_pkts)]) # PNI MI-S2 sensor temperature conversion **GOES HERE**
+
+    # verify checksums
+    checksum_valid = data["calculated_checksum"][range(n_pkts)] == data["CHECKSUM"][range(n_pkts)]
 
     # convert time tags to AstroPy times
     tt2k_epoch = Time('2000-01-01 11:58:55.816', scale='utc')
-    pckt_time = tt2k_epoch + TimeDelta(pckt_times * u.s)
+    pkt_time = tt2k_epoch + TimeDelta(pkt_times * u.s)
     fg_time = tt2k_epoch + TimeDelta(fg_times * u.s)
+
+    # RT clock times
+    rtc_year = bcd_to_int(np.array(data["RTC_YEAR"][range(n_pkts)]))  #  convert from Binary Coded Decimal
+    rtc_month = bcd_to_int(np.array(data["RTC_MONTH"][range(n_pkts)]))
+    rtc_day = bcd_to_int(np.array(data["RTC_DAY"][range(n_pkts)]))
+    rtc_hours = bcd_to_int(np.array(data["RTC_HOURS"][range(n_pkts)]))
+    rtc_minutes = bcd_to_int(np.array(data["RTC_MINUTES"][range(n_pkts)]))
+    rtc_seconds = bcd_to_int(np.array(data["RTC_SECONDS"][range(n_pkts)]))
+    rt_clock = []
+    for pkt in range(n_pkts):
+        rt_clock_time = Time(
+            "20%02d-%02d-%02d %02d:%02d:%02d.000" % (
+                rtc_year[pkt], rtc_month[pkt], rtc_day[pkt], 
+                rtc_hours[pkt], rtc_minutes[pkt], rtc_seconds[pkt]
+            ), 
+            scale='utc'
+        )
+        rt_clock.append(rt_clock_time.gps)
+
+    log.info(f"Creating 10 S/s TimeSeries structure")
 
     # store 10 Sample/Sec (magnetometer cadence) data in AstroPy TimeSeries structures
     magts = TimeSeries(
@@ -311,32 +379,121 @@ def l0_sci_data_to_l1(data: dict, original_filename: Path) -> Path:
       }
    )
 
+    log.info(f"Creating 1/4 S/s TimeSeries structure")
+
     # store 1 Sample/4 Sec (packet cadence) data in AstroPy TimeSeries structures
     pktts = TimeSeries(
-        time=pckt_time,
+        time=pkt_time,
         data={
+            'hermes_nem_pkt_id': u.Quantity(
+                value = data['PKT_ID'][range(n_pkts)],
+                unit='',
+                dtype=np.float32
+            ),
+            'hermes_nem_acq_pos_num': u.Quantity(
+                value = data['ACQ_POS_NUM'][range(n_pkts)],
+                unit='',
+                dtype=np.uint32
+            ),
+            'hermes_nem_xsum': u.Quantity(
+                value = data['XSUM'][range(n_pkts)],
+                unit='',
+                dtype=np.uint8
+            ),
+            'hermes_nem_start_flag': u.Quantity(
+                value = data['START_FLAG'][range(n_pkts)],
+                unit='',
+                dtype=np.uint8
+            ),
+            'hermes_nem_ops_mode': u.Quantity(
+                value = data['OPS_MODE'][range(n_pkts)],
+                unit='',
+                dtype=np.uint8
+            ),
+            'hermes_nem_relay_state': u.Quantity(
+                value = data['RELAY_STATE'][range(n_pkts)],
+                unit='',
+                dtype=np.uint8
+            ),
+            'hermes_nem_rtc_seconds': u.Quantity(
+                value = rtc_seconds,
+                unit='s',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_minutes': u.Quantity(
+                value = rtc_minutes,
+                unit='min',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_hours': u.Quantity(
+                value = rtc_hours,
+                unit='hr',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_day': u.Quantity(
+                value = rtc_day,
+                unit='day',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_month': u.Quantity(
+                value = rtc_month,
+                unit='',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_year': u.Quantity(
+                value = rtc_year,
+                unit='',
+                dtype=np.uint16
+            ),
+            'hermes_nem_rtc_time': u.Quantity(
+                value = rt_clock,
+                unit='s',
+                dtype=np.float64
+            ),
+            'hermes_nem_isr_count': u.Quantity(
+                value = data["ISR_COUNT"][range(n_pkts)],
+                unit='',
+                dtype=np.uint16
+            ),
             'hermes_nem_ebox_temp': u.Quantity(
-            value = t_eb,
-            unit='deg C',
-            dtype=np.float32
+                value = t_eb,
+                unit='deg C',
+                dtype=np.float32
             ),
             'hermes_nem_fg_temp': u.Quantity(
-            value = t_fg,
-            unit='deg C',
-            dtype=np.float32
+                value = t_fg,
+                unit='deg C',
+                dtype=np.float32
             ),
             'hermes_nem_pni1_temp': u.Quantity(
-            value = t_pni1,
-            unit='deg C',
-            dtype=np.float32
+                value = t_pni1,
+                unit='deg C',
+                dtype=np.float32
             ),
             'hermes_nem_pni2_temp': u.Quantity(
-            value = t_pni2,
-            unit='deg C',
-            dtype=np.float32
+                value = t_pni2,
+                unit='deg C',
+                dtype=np.float32
+            ),
+            'hermes_nem_checksum_valid': u.Quantity(
+                value = checksum_valid,
+                unit='',
+                dtype=np.uint8
+            ),
+            'hermes_nem_checksum': u.Quantity(
+                value = data["CHECKSUM"][range(n_pkts)],
+                unit='',
+                dtype=np.uint16
+            ),
+            'hermes_nem_calculated_checksum': u.Quantity(
+                value = data["calculated_checksum"][range(n_pkts)],
+                unit='',
+                dtype=np.uint16
             )
         }
     )
+
+    log.info(f"Creating HermesData structure")
 
     from hermes_core.timedata import HermesData
     from collections import OrderedDict
@@ -347,7 +504,7 @@ def l0_sci_data_to_l1(data: dict, original_filename: Path) -> Path:
     }
 
     # now flesh out the HermesData structure
-    input_attrs = HermesData.global_attribute_template("nemisis", "l1", "1.0.0")
+    input_attrs = HermesData.global_attribute_template("nemisis", "l1", cdf_version)
 
     nemisis_data = HermesData(
         timeseries=ts,
@@ -398,15 +555,76 @@ def l0_sci_data_to_l1(data: dict, original_filename: Path) -> Path:
         OrderedDict({"CATDESC":"PNI2 sensor temperature in deg C"})
     )
 
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_pkt_id"].meta.update(
+        OrderedDict({"CATDESC":"packet ID"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_acq_pos_num"].meta.update(
+        OrderedDict({"CATDESC":"Acquisition position number"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_xsum"].meta.update(
+        OrderedDict({"CATDESC":"MOSI Packet Checksum Comparison Agreed"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_start_flag"].meta.update(
+        OrderedDict({"CATDESC":"Power-On Start Flag"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_ops_mode"].meta.update(
+        OrderedDict({"CATDESC":"Last Packet before shutdown"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_relay_state"].meta.update(
+        OrderedDict({"CATDESC":"Transorb Relay Engaged"})
+    )
+
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_seconds"].meta.update(
+        OrderedDict({"CATDESC":"RTC seconds"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_minutes"].meta.update(
+        OrderedDict({"CATDESC":"RTC minutes"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_hours"].meta.update(
+        OrderedDict({"CATDESC":"RTC hours"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_day"].meta.update(
+        OrderedDict({"CATDESC":"RTC day"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_month"].meta.update(
+        OrderedDict({"CATDESC":"RTC month"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_year"].meta.update(
+        OrderedDict({"CATDESC":"RTC year"})
+    )
+
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_rtc_time"].meta.update(
+        OrderedDict({"CATDESC":"RTC time as a GPS time"})
+    )
+
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_isr_count"].meta.update(
+        OrderedDict({"CATDESC":"Interrupt service request count"})
+    )
+
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_checksum_valid"].meta.update(
+        OrderedDict({"CATDESC":"telemetry packet checksum valid"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_checksum"].meta.update(
+        OrderedDict({"CATDESC":"telemetry packet checksum"})
+    )
+    nemisis_data.timeseries["Epoch_pkt"]["hermes_nem_calculated_checksum"].meta.update(
+        OrderedDict({"CATDESC":"telemetry packet calculated checksum"})
+    )
+    log.info(f"Finished Creating HermesData structure")
 
     cdf_file_path = nemisis_data.save(output_path=hermes_nemisis._data_directory, overwrite=True)
+
+    log.info(f"Wrote l1 cdf file:{cdf_file_path.name}.")
+
     return cdf_file_path
-    # 
-#    cdf_file_path = nemisis_data.save(output_path=cdf_filename, overwrite=True)
 
+def bcd_to_int(bcd):
+    """
+    Given a byte in Binary Coded Decimal, return the decoded value
+    will work for a scalar input or a numpy array of input values
+    """
 
-#    return cdf_filename
-
+    return (bcd >> 4) * 10 + (bcd & 0xF)
 
 def get_calibration_file(data_filename: Path, time=None) -> Path:
     """
